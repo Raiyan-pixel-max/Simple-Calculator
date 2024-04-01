f2f714c2c8253829cb922afcf7281a34900a0d8a
rayyanahmed0 <ahmedraiyan084@gmail.com  git config --global user.name rayyanahmed0   git config --global user.email ahmedraiyan084@gmail.com>
2024-04-01 20:35:46 +0500

    Adding a Simple CLI Calculator Project

---
+++ calculator.ts
@@ -0,0 +1,31 @@
+import inquirer from "inquirer"; 
+
+console.log("Welcome to Raiyan's Simple Calculator");
+
+
+const answer = await inquirer.prompt([
+    {massage: "Enter your first number",type: "number", name : "firstNumber"},
+    {massage: "Enter your first number",type: "number", name : "secondNumber"},
+    {massage : "Select an operator to perform action", type: "list", name: "operator", choices: ["Add", "Subtract", "Multiply", "Divide"] }
+
+
+])
+    if (answer.operator === "Add"){
+        console.log(answer.firstNumber + answer.secondNumber);
+    }
+    
+    else if (answer.operator === "Subtract"){
+        console.log(answer.firstNumber - answer.secondNumber);
+    }
+
+    else if (answer.operator === "Multiply"){
+        console.log(answer.firstNumber * answer.secondNumber);
+    }
+
+    else if (answer.operator === "Divide"){
+        console.log(answer.firstNumber / answer.secondNumber);
+    }
+
+else("Invalid Input");
+
+
